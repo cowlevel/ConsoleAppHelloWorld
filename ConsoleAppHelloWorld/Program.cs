f1c4a52520807eb37c5682444bf55e62a160a2ec
cowlevel07 <cyrjillcanon07@gmail.com>
2019-02-04 03:29:53 +0800

    added a comment

--- ConsoleAppHelloWorld/Program.cs
+++ ConsoleAppHelloWorld/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAppHelloWorld
 {
@@ -10,6 +6,11 @@ namespace ConsoleAppHelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+
+            //  a comment
         }
     }
 }
